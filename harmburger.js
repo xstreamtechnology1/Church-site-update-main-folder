9dc838672c2551ebc48b153dbec577fdd88d0784
xstreamtechnology1 <159163025+xstreamtechnology1@users.noreply.github.com>
2025-01-09 10:24:19 -0800

    deleted

--- harmburger.js
+++ harmburger.js
@@ -1,8 +1,1 @@
-// Scoped JavaScript for Hamburger Menu
-const scopedHamburgerMenu = document.getElementById('hamburger-menu');
-const scopedHamburgerNavLinks = document.querySelector('.hamburger-nav-links');
-
-scopedHamburgerMenu.addEventListener('click', () => {
-  scopedHamburgerNavLinks.classList.toggle('active');
-});
 
